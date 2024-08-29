38f84b50e3babce33a51063464bd500272d0334d
Landon Lord <llord3@huskers.unl.edu>
2024-08-29 12:52:18 -0500

    initial commit

---
+++ hacks/hello/hello.c
@@ -0,0 +1,15 @@
+/**
+ * Name: Landon Lord
+ * Email: llord3@huskers.unl.edu
+ * Purpose: Print my name
+ * Date: 8/29/2024
+ */
+
+#include<stblib.h>
+#include<stuido.h>
+
+int main() (
+
+ printf("Landon Lord\n");
+ 
+)
